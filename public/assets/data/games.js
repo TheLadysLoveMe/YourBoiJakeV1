a0c181e8538cf968f79fed9875d6befc7c477174
TheLadysLoveMe <142649696+TheLadysLoveMe@users.noreply.github.com>
2024-03-06 10:29:15 -0500

    Update games.js

--- public/assets/data/games.js
+++ public/assets/data/games.js
@@ -31,10 +31,10 @@ var games = [
     url: 'https://images-opensocial.googleusercontent.com/gadgets/ifr?url=https://738501629-461082748261058427.preview.editmysite.com/uploads/b/139890129-817510652323129407/files/drbo.xml'
   },
   {
-    id: 'escapeprison',
-    title: 'Henry Stickmin: Escaping the Prison',
-    url: 'https://rawcdn.githack.com/3kh0/3kh0-Assets/e3144855e924629d8f7c41238660a823a5336705/escapingtheprison/index.html',
-    image: 'https://www.allbusinesstemplates.com/thumbs/8757c0ff-ca9a-46d3-b009-a1c0113bdcfa_1.png'
+    id: 'sushiroll',
+    title: 'Sushi Unroll',
+    url: 'https://ehab.helooo.com/games/sushi-unroll/index.html',
+    image: 'https://ehab.helooo.com/img/SUSHIunROLL.png'
   },
   {
     id: 'breakingthebank',
