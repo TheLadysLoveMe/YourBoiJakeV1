8bfc72e4e60d869de01c6a087c6ca3ee42d42c8c
TheLadysLoveMe <142649696+TheLadysLoveMe@users.noreply.github.com>
2024-03-06 22:42:50 -0500

    Update games.js

--- public/assets/data/games.js
+++ public/assets/data/games.js
@@ -100,14 +100,14 @@ var games = [
     id: '1v1lol',
     title: "1v1.lol",
     url: 'https://nowggnowggnowggnowggnowggnowggnowggnowggnowggnowggnowggnowgg.gg.coffe.ink/lessons?pg=449#f18k3kkvo78luou',
-    image: 'https://www.allbusinesstemplates.com/thumbs/8757c0ff-ca9a-46d3-b009-a1c0113bdcfa_1.png'
+    image: 'https://tse2.mm.bing.net/th?id=OIP.W4iXiVKmFVKt9y99It-wXwHaFt&pid=Api&P=0&h=180'
   },
   { 
     id: 'roblox',
     title: 'Roblox',
-    url: 'https://now.gg/play/roblox-corporation/5349/roblox',
+    url: 'https://educationbluesky.com/',
     image: 'https://play-lh.googleusercontent.com/WNWZaxi9RdJKe2GQM3vqXIAkk69mnIl4Cc8EyZcir2SKlVOxeUv9tZGfNTmNaLC717Ht=w240-h480-rw',
-    description: 'EXPERIMENTAL: This may or may not work for you. Please don\'t report it if it doesn\'t. If you have bad WiFi, it may load slower than usual.'
+    description: 'EXPERIMENTAL: This may or may not work for you. Please don\'t report it if it doesn\'t. If you have bad WiFi, it may load slower than usual (Advice: This works better at your house).'
   },
   {
     id: 'recoil',
@@ -119,7 +119,7 @@ var games = [
     id: 'retrobowl',
     title: 'Retro Bowl',
     url: 'https://googleads.g.doubleclick.net/pagead/ads?client=ca-pub-1434808215804214&output=html&h=599&slotname=6227742594&adk=2042780945&adf=521995924&pi=t.ma~as.6227742594&w=256&lmt=1709738651&rafmt=12&format=256x599&url=https%3A%2F%2Fehab.helooo.com%2Fgameurl%3Fgameid%3D%24439804&wgl=1&uach=WyJXaW5kb3dzIiwiMTAuMC4wIiwieDg2IiwiIiwiMTIyLjAuNjI2MS45NSIsbnVsbCwwLG51bGwsIjY0IixbWyJDaHJvbWl1bSIsIjEyMi4wLjYyNjEuOTUiXSxbIk5vdChBOkJyYW5kIiwiMjQuMC4wLjAiXSxbIkdvb2dsZSBDaHJvbWUiLCIxMjIuMC42MjYxLjk1Il1dLDBd&dt=1709738651519&bpp=1&bdt=131&idt=58&shv=r20240305&mjsv=m202402290101&ptt=9&saldr=aa&abxe=1&prev_fmts=0x0%2C1428x90&nras=1&correlator=7748460110662&frm=20&pv=1&ga_vid=290450757.1709738314&ga_sid=1709738652&ga_hid=697212201&ga_fc=1&u_tz=-300&u_his=12&u_h=1080&u_w=1920&u_ah=1040&u_aw=1920&u_cd=24&u_sd=1&dmc=8&adx=1200&ady=326&biw=1910&bih=919&scr_x=0&scr_y=0&eid=44759876%2C44759927%2C44759842%2C42532524%2C95322748%2C95325752%2C95326315%2C95327082%2C95324161%2C95325785%2C95326918%2C31078663%2C31078665%2C31078668%2C31078670&oid=2&pvsid=3945083062266714&tmod=757157770&uas=0&nvt=1&ref=https%3A%2F%2Fehab.helooo.com%2Fallgames&fc=1920&brdim=0%2C0%2C0%2C0%2C1920%2C0%2C1920%2C1040%2C1920%2C919&vis=1&rsz=%7C%7CoeE%7C&abl=CS&pfx=0&fu=256&bc=31&bz=1&td=1&psd=W251bGwsbnVsbCxudWxsLDFd&nt=1&ifi=3&uci=a!3&fsb=1&dtd=62',
-    image: 'https://www.allbusinesstemplates.com/thumbs/8757c0ff-ca9a-46d3-b009-a1c0113bdcfa_1.png'
+    image: 'https://tse2.mm.bing.net/th?id=OIP.W4iXiVKmFVKt9y99It-wXwHaFt&pid=Api&P=0&h=180'
   },
   {
     id: 'brawlguys',
