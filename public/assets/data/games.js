2f5515a98df2abbb5302504c5d1a1b3052279ef4
TheLadysLoveMe <142649696+TheLadysLoveMe@users.noreply.github.com>
2024-03-08 21:52:28 -0500

    Update games.js

--- public/assets/data/games.js
+++ public/assets/data/games.js
@@ -105,7 +105,7 @@ var games = [
   { 
     id: 'retrobowl',
     title: 'Retro Bowl',
-    url: 'https://game316009.konggames.com/gamez/0031/6009/live/index.html',
+    url: 'gamescode/retrobowl.html',
     image: 'https://tse2.mm.bing.net/th?id=OIP.W4iXiVKmFVKt9y99It-wXwHaFt&pid=Api&P=0&h=180'
   },
   {
