52b5d8ba947bdb263a5f9a007dffc1d884f8be83
TheLadysLoveMe <142649696+TheLadysLoveMe@users.noreply.github.com>
2024-03-11 00:17:36 -0400

    Add files via upload

---
+++ public/assets/games.js
@@ -0,0 +1,92 @@
+[
+  {
+    "link": "projects/1/index.html",
+    "imgSrc": "projects/1/meta/apple-touch-icon.png",
+    "title": "1"
+  },
+  {
+    "link": "projects/1v1space/index.html",
+    "imgSrc": "projects/1v1space/splash.png",
+    "title": "1v1.space"
+  },
+  {
+    "link": "projects/2048/index.html",
+    "imgSrc": "projects/2048/thumb.png",
+    "title": "2048"
+  },
+  {
+    "link": "projects/basketball-stars/index.html",
+    "imgSrc": "projects/basketball-stars/assets/images/basketball-stars.png",
+    "title": "Basketball Stars"
+  },
+  {
+    "link": "projects/bloxors/index.html",
+    "imgSrc": "projects/bloxors/block.png",
+    "title": "Bloxors"
+  },
+  {
+    "link": "projects/chrome-dino/index.html",
+    "imgSrc": "projects/chrome-dino/icons/icon-256.png",
+    "title": "Chrome Dino"
+  },
+  {
+    "link": "projects/cookie-clicker/index.html",
+    "imgSrc": "projects/cookie-clicker/cookie1.jpeg",
+    "title": "Cookie Clicker"
+  },
+  {
+    "link": "projects/cupcake2048/index.html",
+    "imgSrc": "projects/cupcake2048/favicon.png",
+    "title": "Cupcake 2048"
+  },
+  {
+    "link": "projects/fake-virus/icon.png",
+    "imgSrc": "projects/fake-virus/icon.png",
+    "title": "Fake Virus"
+  },
+  {
+    "link": "projects/flappy-2048/index.html",
+    "imgSrc": "projects/flappy-2048/meta/apple-touch-icon.png",
+    "title": "Flappy 2048"
+  },
+  {
+    "link": "projects/flappy-bird/index.html",
+    "imgSrc": "projects/flappy-bird/assets/thumb.png",
+    "title": "Flappy Bird"
+  },
+  {
+    "link": "projects/geodash/index.html",
+    "imgSrc": "projects/geodash/geoscratchicon.png",
+    "title": "Geometry Dash (Scratch)"
+  },
+  {
+    "link": "projects/getaway-shootout/index.html",
+    "imgSrc": "projects/getaway-shootout/img/images.jpg",
+    "title": "Getaway Shootout"
+  },
+  {
+    "link": "projects/hackertype/index.html",
+    "imgSrc": "projects/hackertype/logo192.png",
+    "title": "Hacker Typer"
+  },
+  {
+    "link": "projects/retro-bowl/index.html",
+    "imgSrc": "projects/retro-bowl/img/icon.jpg",
+    "title": "Retro Bowl"
+  },
+  {
+    "link": "projects/slope/index.html",
+    "imgSrc": "projects/slope/slope4.jpeg",
+    "title": "Slope"
+  },
+  {
+    "link": "projects/sort-the-court/index.html",
+    "imgSrc": "projects/sort-the-court/img/splash.png",
+    "title": "Sort The Court"
+  },
+  {
+    "link": "projects/1v1lol/index.html",
+    "imgSrc": "projects/1v1lol/splash.png",
+    "title": "1v1.lol"
+  }
+]
