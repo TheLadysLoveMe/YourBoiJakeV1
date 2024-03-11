7f162e07a285e6495b04da67db37094a40f2701c
TheLadysLoveMe <142649696+TheLadysLoveMe@users.noreply.github.com>
2024-03-11 00:22:10 -0400

    Update games.js

--- public/assets/data/games.js
+++ public/assets/data/games.js
@@ -1,92 +1,152 @@
-[
+var games = [
   {
-    "link": "projects/1/index.html",
-    "imgSrc": "projects/1/meta/apple-touch-icon.png",
-    "title": "1"
+    id: 'customgame',
+    title: 'Add custom game',
+    image: 'assets/images/add.png'
   },
   {
-    "link": "projects/1v1space/index.html",
-    "imgSrc": "projects/1v1space/splash.png",
-    "title": "1v1.space"
+    id: 'cookieclicker',
+    title: 'Cookie Clicker',
+    description: 'Cookie Clicker is an addictive incremental game where players click to produce cookies, unlock upgrades, and aim to amass an ever-growing mountain of delicious digital treats.',
+    url: 'https://sushi8756.github.io/Cookie-Clicker-2.031/',
+    image: 'https://cdn.discordapp.com/icons/339131488111034389/73f15cd93d96504af9877a53816d110c.png'
   },
   {
-    "link": "projects/2048/index.html",
-    "imgSrc": "projects/2048/thumb.png",
-    "title": "2048"
+    id: 'fbwg',
+    title: 'Fireboy and Watergirl',
+    description: 'A 2 player game where you have to work together using logic and teamwork to get to the finish line.',
+    url: 'https://educationgg-23704.web.app/games/FBWG4-main/index.html',
+    image: 'https://play-lh.googleusercontent.com/5-YcjiTyMrPJEZe078dHMxnCxcg2A_8NWJZQieRvn2Gkf8JOuHZEbTmFyQ9gn5p2lnU'
   },
   {
-    "link": "projects/basketball-stars/index.html",
-    "imgSrc": "projects/basketball-stars/assets/images/basketball-stars.png",
-    "title": "Basketball Stars"
+    id: 'bloxerz',
+    title: 'Bloxerz',
+    url: 'https://www.ehabgames.com/games/bloxors/index.html',
+    image: 'https://th.bing.com/th?id=OIP.6jjUQ7Niv2cl4NvkOrkf_QAAAA&w=255&h=244&c=8&rs=1&qlt=90&o=6&pid=3.1&rm=2'
   },
   {
-    "link": "projects/bloxors/index.html",
-    "imgSrc": "projects/bloxors/block.png",
-    "title": "Bloxors"
+    id: 'driftboss',
+    title: 'Drift Boss',
+    image: 'https://play-lh.googleusercontent.com/fLrnXuD1B0w6FEDJ1UKmv_TYbNyJ4GwzsQu9ltx57MfIj037nig4cVunG3CYmczkjMDA',
+    url: 'https://images-opensocial.googleusercontent.com/gadgets/ifr?url=https://738501629-461082748261058427.preview.editmysite.com/uploads/b/139890129-817510652323129407/files/drbo.xml'
   },
   {
-    "link": "projects/chrome-dino/index.html",
-    "imgSrc": "projects/chrome-dino/icons/icon-256.png",
-    "title": "Chrome Dino"
+    id: 'breakingthebank',
+    title: 'Henry Stickmin: Breaking the Bank',
+    url: 'https://art-class.github.io/assets/breakingthebank/index.html',
+    image: 'https://play-lh.googleusercontent.com/I9aDLwmiy5iSjeUPs5IDI3lam8iWZ1zVnLF-V_EGvClOLuccjwLNNjZ8xltu4AN60io'
   },
   {
-    "link": "projects/cookie-clicker/index.html",
-    "imgSrc": "projects/cookie-clicker/cookie1.jpeg",
-    "title": "Cookie Clicker"
+    id: '2048',
+    title: '2048',
+    url: 'https://www.2048.org/',
+    image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/1/18/2048_logo.svg/220px-2048_logo.svg.png'
   },
   {
-    "link": "projects/cupcake2048/index.html",
-    "imgSrc": "projects/cupcake2048/favicon.png",
-    "title": "Cupcake 2048"
+    id: 'googlesnake',
+    title: 'Google Snake',
+    url: 'https://www.google.com/fbx?fbx=snake_arcade',
+    image: 'https://th.bing.com/th/id/OIP.-Bdzvy87SmMmkN8X8si0mAHaFR?w=254&h=180&c=7&r=0&o=5&pid=1.7'
   },
   {
-    "link": "projects/fake-virus/icon.png",
-    "imgSrc": "projects/fake-virus/icon.png",
-    "title": "Fake Virus"
+    id: 'slow roads',
+    title: 'Slow Roads',
+    url: 'https://slowroads.io/',
+    image: 'https://slowroads.io/static/media/splash-logo-placeholder-7b.2d60164d.png'
   },
   {
-    "link": "projects/flappy-2048/index.html",
-    "imgSrc": "projects/flappy-2048/meta/apple-touch-icon.png",
-    "title": "Flappy 2048"
+    id: 'littlealchemy',
+    title: 'Little Alchemy',
+    url: 'https://littlealchemy.com',
+    image: 'https://play-lh.googleusercontent.com/-NQ2QLAOaafy7iHAJcrdEFk-tyVicnbwy6iLehml8K0Higi60sSVn0829bcyi1lOUw'
+   },
+  {
+    id: 'littlealchemy2',
+    title: 'Little Alchemy 2',
+    url: 'https://littlealchemy2.com/',
+    image: 'https://hints.littlealchemy2.com/img/la2-logo.svg' 
   },
   {
-    "link": "projects/flappy-bird/index.html",
-    "imgSrc": "projects/flappy-bird/assets/thumb.png",
-    "title": "Flappy Bird"
+    id: 'chromedino',
+    title: 'ChromeDino',
+    url: 'https://chrome-dino.com/',
+    image: 'https://th.bing.com/th/id/OIP.Cyx-tvXuVrSjk-U33dQdWgHaHa?w=178&h=180&c=7&r=0&o=5&pid=1.7'
   },
   {
-    "link": "projects/geodash/index.html",
-    "imgSrc": "projects/geodash/geoscratchicon.png",
-    "title": "Geometry Dash (Scratch)"
+    id: 'soundboard',
+    title: 'SoundBoard',
+    url: 'https://www.myinstants.com/categories/sound%20effects/',
+    image: 'https://th.bing.com/th?id=ODLS.a14279a1-2396-4c5e-a1d2-a7062e7ae498&w=32&h=32&qlt=90&pcl=fffffa&o=6&pid=1.2'
   },
   {
-    "link": "projects/getaway-shootout/index.html",
-    "imgSrc": "projects/getaway-shootout/img/images.jpg",
-    "title": "Getaway Shootout"
+    id: 'geometrydash',
+    title: 'Geometry Dash',
+    url: 'https://www.phantomgames.dev/game/geometry-dash-remastered/index.html',
+    image: 'https://upload.wikimedia.org/wikipedia/en/3/35/Geometry_Dash_Logo.PNG'
   },
   {
-    "link": "projects/hackertype/index.html",
-    "imgSrc": "projects/hackertype/logo192.png",
-    "title": "Hacker Typer"
+    id: 'swerve',
+    title: 'Swerve',
+    url: 'https://s3-eu-west-1.amazonaws.com/apps.playcanvas.com/VecAxR3e/index.html',
+    image: 'https://th.bing.com/th/id/OIP.CFRRtBXXyH_FP7XGCJwMdQAAAA?w=172&h=176&c=7&r=0&o=5&pid=1.7'
   },
   {
-    "link": "projects/retro-bowl/index.html",
-    "imgSrc": "projects/retro-bowl/img/icon.jpg",
-    "title": "Retro Bowl"
+    id: 'baskletballstars',
+    title: 'Basketball Stars',
+    url: 'https://basketball-stars.pro/g2/bbstars/',
+    image: 'https://th.bing.com/th/id/OIP.Jh8m7AFwi2ApbNbXY2qe3gAAAA?w=219&h=180&c=7&r=0&o=5&pid=1.7'
   },
   {
-    "link": "projects/slope/index.html",
-    "imgSrc": "projects/slope/slope4.jpeg",
-    "title": "Slope"
+    id: '1v1lol',
+    title: "1v1.lol",
+    url: 'https://nowggnowggnowggnowggnowggnowggnowggnowggnowggnowggnowggnowgg.gg.coffe.ink/lessons?pg=449#f18k3kkvo78luou',
+    image: 'https://tse2.mm.bing.net/th?id=OIP.W4iXiVKmFVKt9y99It-wXwHaFt&pid=Api&P=0&h=180'
+  },
+  { 
+    id: 'retrobowl',
+    title: 'Retro Bowl',
+    url: 'gamescode/retrobowl.html',
+    image: 'https://tse2.mm.bing.net/th?id=OIP.W4iXiVKmFVKt9y99It-wXwHaFt&pid=Api&P=0&h=180'
   },
   {
-    "link": "projects/sort-the-court/index.html",
-    "imgSrc": "projects/sort-the-court/img/splash.png",
-    "title": "Sort The Court"
+    id: 'roblox',
+    title: 'Roblox',
+    url: 'https://educationbluesky.com/',
+    image: 'https://play-lh.googleusercontent.com/WNWZaxi9RdJKe2GQM3vqXIAkk69mnIl4Cc8EyZcir2SKlVOxeUv9tZGfNTmNaLC717Ht=w240-h480-rw',
+    description: 'EXPERIMENTAL: This may or may not work for you. Please don\'t report it if it doesn\'t. If you have bad WiFi, it may load slower than usual (Advice: This works better at your house).'
   },
   {
-    "link": "projects/1v1lol/index.html",
-    "imgSrc": "projects/1v1lol/splash.png",
-    "title": "1v1.lol"
+    id: 'recoil',
+    title: 'Recoil',
+    url: 'https://rawcdn.githack.com/nightrose-labs/quartz/b0687b5099af0b76aedf37743f2d03cbeef26467/2/5cf218d2-f550-420c-8e55-7029efc5b4db/index.html',
+    image: 'https://rawcdn.githack.com/nightrose-labs/quartz/master/thumb/5cf218d2-f550-420c-8e55-7029efc5b4db.jpg'
+  },
+  {
+    id: 'brawlguys',
+    title: 'Brawl Guys',
+    url: 'https://smartfoloosanta.bitbucket.io/retro-bowl/',
+    image: 'https://play-lh.googleusercontent.com/EiElcSrd6-o-19roiswSx0AZPzsq6qF3hUGHsSWDl5UVtj7G23DHkneM8ucwqyOmEg'
+  }
+]
+games.sort(function (a, b) {
+  return a.title.localeCompare(b.title);
+});
+var customGameId = "customgame"; // replace with the id of the game you want to pin
+var customGame = games.find(function (game) {
+  return game.id === customGameId;
+});
+games.splice(games.indexOf(customGame), 1);
+games.unshift(customGame);
+
+var customgames = getObj("customgames")
+if (customgames) customgames.forEach(game => {
+  games.push(game)
+})
+
+console.log("Loaded " + games.length + " games")
+if (customgames) console.log("Loaded " + customgames.length + " custom games")
+
+console.log("Loaded " + games.length + " games")
+if (customgames) console.log("Loaded " + customgames.length + " custom games")
   }
 ]
